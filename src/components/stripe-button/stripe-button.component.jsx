ac0bb7d03ccdf2f672dd84db912398eea01f8287
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2020-11-11 11:25:11 +0100

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51HmFDkK8xbqILmn8YhdrQIl6Sv5SjwVtmECvFW3vjtfeZ44iYUkGxXML10JMCyvVF673fUrW0FO4sj5zNWa2RuwV00IGZYDz1m';
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='J.A E-commerce'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
