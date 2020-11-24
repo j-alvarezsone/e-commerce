dcf9d6e1deacae1603c123dddc11fe43115942f5
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2020-11-24 20:30:11 +0100

    updating our code to be pwa compliant, added serviceworker, https, updated manifest

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var enforce = require('express-sslify');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use(cors());
 
@@ -30,6 +32,10 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, (error) => {
   if (error) throw error;
   console.log(`Server running on port ${port}`);
+});
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
